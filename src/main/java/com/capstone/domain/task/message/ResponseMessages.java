bfcbd9c125e703a0558dae582a5aa667b8a65ebd
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-23 23:51:02 +0900

    FEAT(message): 응답 메시지 및 상태값 설정

---
+++ src/main/java/com/capstone/domain/task/message/ResponseMessages.java
@@ -0,0 +1,12 @@
+package com.capstone.domain.task.message;
+
+public class ResponseMessages {
+    public static final String TASK_CREATED = "작업 생성이 성공적으로 완료되었습니다.";
+    public static final String TASK_UPDATED = "업데이트가 성공적으로 완료되었습니다.";
+    public static final String TASK_DROPPED = "작업 삭제가 성공적으로 완료되었습니다.";
+    public static final String VERSION_ADDED = "작업에 새로운 버전이 추가되었습니다.";
+    public static final String STATUS_UPDATED = "상태 변경이 성공적으로 완료되었습니다.";
+    public static final String VERSION_ROLLBACKED = "버전 롤백이 성공적으로 완료되었습니다.";
+    public static final String TASK_NOT_FOUND = "업데이트 하고자 하는 작업은 기존에 없던 작업 입니다.";
+    public static final String VERSION_NOT_FOUND = "롤백하고자 하는 버전은 기존에 없던 버전 입니다.";
+}
