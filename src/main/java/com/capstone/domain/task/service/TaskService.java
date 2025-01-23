4239dd49d6fd9d9c4c64602e098cf6e35e6039d1
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-23 23:53:32 +0900

    FEAT: 버전 추가, 롤백, 불러오기 메서드 추가

--- src/main/java/com/capstone/domain/task/service/TaskService.java
+++ src/main/java/com/capstone/domain/task/service/TaskService.java
@@ -1,19 +1,17 @@
-package com.capstone.task.service;
+package com.capstone.domain.task.service;
 
-import com.capstone.task.dto.TaskDto;
-import com.capstone.task.entity.Task;
-import com.capstone.task.entity.Version;
-import com.capstone.task.exception.TaskNotFoundException;
-import com.capstone.task.message.ResponseMessages;
-import com.capstone.task.repository.TaskRepository;
-import com.capstone.task.util.TaskUtil;
+import com.capstone.domain.task.dto.TaskDto;
+import com.capstone.domain.task.entity.Task;
+import com.capstone.domain.task.entity.Version;
+import com.capstone.domain.task.exception.TaskNotFoundException;
+import com.capstone.domain.task.exception.VersionNotFoundException;
+import com.capstone.domain.task.message.ResponseMessages;
+import com.capstone.domain.task.repository.TaskRepository;
+import com.capstone.domain.task.util.TaskUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,10 +22,22 @@ public class TaskService {
         taskRepository.save(TaskUtil.toEntity(taskDto));
         return ResponseMessages.TASK_CREATED;
     }
-    public String updateTask(TaskDto taskDto){
-        Version version = TaskUtil.toVersion(taskDto);
-        Task task = findTaskByIdOrThrow(taskDto.getId());
+    public Version loadVersionContent(String taskId) {
+        Task task = findTaskByIdOrThrow(taskId);
+        String currentVersion = task.getCurrentVersion();
 
+        return task.getVersionHistory().stream()
+                .filter(version -> version.getVersion().equals(currentVersion))
+                .findFirst()
+                .orElseThrow(() -> new VersionNotFoundException(ResponseMessages.VERSION_NOT_FOUND));
+    }
+    public String addVersion(TaskDto taskDto){
+        Version version = TaskUtil.createVersion(taskDto);
+        Task task = findTaskByIdOrThrow(taskDto.getId());
+        task.getVersionHistory().add(version);
+        taskRepository.save(task);
+
+        return ResponseMessages.VERSION_ADDED;
     }
     public String dropTask(String id){
         Task task = findTaskByIdOrThrow(id);
@@ -42,8 +52,20 @@ public class TaskService {
         return ResponseMessages.STATUS_UPDATED;
     }
 
+    public List<Version> listVersions(String taskId){
+        Task task = findTaskByIdOrThrow(taskId);
+        return task.getVersionHistory();
+    }
+
+    public Task rollbackVersion(String taskId, String version){
+        Task task = findTaskByIdOrThrow(taskId);
+        task.setCurrentVersion(version);
+        taskRepository.save(task);
+        return task;
+    }
+
     public Task findTaskByIdOrThrow(String id){
         return taskRepository.findById(id)
-                .orElseThrow(() -> new TaskNotFoundException("존재하지 않는 작업 ID: " + id));
+                .orElseThrow(() -> new TaskNotFoundException(ResponseMessages.TASK_NOT_FOUND));
     }
 }
