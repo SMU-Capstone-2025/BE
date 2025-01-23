0474f6a570c38be04e743455fd2647ee78902046
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-23 23:54:54 +0900

    FEAT: 날짜 형식 변경 메서드를 유틸리티 클래스에 포함

---
+++ src/main/java/com/capstone/global/util/DateUtil.java
@@ -0,0 +1,12 @@
+package com.capstone.global.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static String getCurrentFormattedDateTime() {
+        return LocalDateTime.now().format(FORMATTER);
+    }
+}
