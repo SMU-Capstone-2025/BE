836f9093ff165b67455fea458a5a00ade12c353b
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-24 11:16:43 +0900

    REFACTOR(repo): 커스텀 레포지토리 상속

--- src/main/java/com/capstone/domain/task/repository/TaskRepository.java
+++ src/main/java/com/capstone/domain/task/repository/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.capstone.domain.task.repository;
 import com.capstone.domain.task.entity.Task;
+import com.capstone.domain.task.repository.custom.CustomTaskRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends MongoRepository<Task, String> {
+public interface TaskRepository extends MongoRepository<Task, String>, CustomTaskRepository {
 }
