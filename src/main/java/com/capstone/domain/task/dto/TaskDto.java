ff7cd3fbf5ffe79700e8748680df20bc02ca084d
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-26 21:29:51 +0900

    STYLE(annotation): JsonIgnoreProperties 어노테이션 추가

--- src/main/java/com/capstone/domain/task/dto/TaskDto.java
+++ src/main/java/com/capstone/domain/task/dto/TaskDto.java
@@ -1,10 +1,16 @@
 package com.capstone.domain.task.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.annotation.Nullable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TaskDto {
     @Nullable // 신규 작성 시에는 없음.
     private String id; // taskId
