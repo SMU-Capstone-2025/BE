b0fea5295a1e1d22f9ab8781cfcff7dddd039fa1
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-27 20:50:44 +0900

    FEAT(repo): 동적 쿼리를 실행하는 커스텀 레포지토리

--- src/main/java/com/capstone/global/elastic/repository/LogRepository.java
+++ src/main/java/com/capstone/global/elastic/repository/LogRepository.java
@@ -1,7 +1,8 @@
 package com.capstone.global.elastic.repository;
 
 import com.capstone.global.elastic.entity.LogEntity;
+import com.capstone.global.elastic.repository.custom.CustomLogRepository;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface LogRepository extends ElasticsearchRepository<LogEntity, String> {
+public interface LogRepository extends ElasticsearchRepository<LogEntity, String>, CustomLogRepository {
 }
