92bb5d2925f787ef5576ed9ea068aa3f249b0675
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-30 22:07:01 +0900

    FIX: 버전 생성 방식 수정

--- src/main/java/com/capstone/domain/task/util/TaskUtil.java
+++ src/main/java/com/capstone/domain/task/util/TaskUtil.java
@@ -1,15 +1,25 @@
 package com.capstone.domain.task.util;
 
 import com.capstone.domain.task.message.Status;
+import com.capstone.domain.task.repository.TaskRepository;
 import com.capstone.global.util.DateUtil;
 import com.capstone.domain.task.dto.TaskDto;
 import com.capstone.domain.task.entity.Task;
 import com.capstone.domain.task.entity.Version;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class TaskUtil {
-    public static Task toEntity(TaskDto taskDto) {
+    private final TaskRepository taskRepository;
+
+    public Task toEntity(TaskDto taskDto) {
         return Task.builder()
                 .title(taskDto.getTitle())
                 .status(Status.IN_PROGRESS)
@@ -18,15 +28,23 @@ public class TaskUtil {
                 .build();
     }
 
-    public static Version createVersion(TaskDto taskDto) {
-        return Version.builder()
-                .taskId(taskDto.getId())
-                .version(taskDto.getVersion())
-                .modifiedDateTime(DateUtil.getCurrentFormattedDateTime())
-                .modifiedBy(taskDto.getModifiedBy())
-                .summary(taskDto.getSummary())
-                .content(taskDto.getContent())
-                .build();
+    public Version createOrGetVersion(TaskDto taskDto, String fileId) {
+        Version version = taskRepository.findByTaskIdAndVersion(taskDto.getId(), taskDto.getVersion());
+        if (version == null){
+            List<String> attachmentList = new ArrayList<>(); // 새로운 리스트 생성
+            attachmentList.add(fileId);
+            return Version.builder()
+                    .taskId(taskDto.getId())
+                    .version(taskDto.getVersion())
+                    .modifiedDateTime(DateUtil.getCurrentFormattedDateTime())
+                    .modifiedBy(taskDto.getModifiedBy())
+                    .summary(taskDto.getSummary())
+                    .content(taskDto.getContent())
+                    .attachmentList(attachmentList)
+                    .build();
+        }
+        version.getAttachmentList().add(fileId);
+        return version;
     }
 
 }
