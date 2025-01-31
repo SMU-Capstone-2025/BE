6389886c3c0e8e0b3b6100600db1d6123bac7083
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-01-31 22:43:57 +0900

    FEAT: 예외 처리 시 띄울 메시지

---
+++ src/main/java/com/capstone/domain/file/common/FileMessages.java
@@ -0,0 +1,7 @@
+package com.capstone.domain.file.common;
+
+public class FileMessages {
+    public static final String FILE_NOT_SUPPORTED = "지원하지 않는 파일 형식입니다.";
+    public static final String FILE_NOT_FOUND = "파일을 찾을 수 없습니다.";
+    public static final String FILE_EMPTY = "파일이 비어있습니다.";
+}
--- src/main/java/com/capstone/domain/file/common/FileTypes.java
+++ src/main/java/com/capstone/domain/file/common/FileTypes.java
@@ -1,5 +1,7 @@
 package com.capstone.domain.file;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public class FileTypes {
@@ -7,4 +9,7 @@ public class FileTypes {
     public static final List<String> SUPPORTED_DOCUMENT_TYPES = List.of("application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain");
     public static final List<String> SUPPORTED_SPREADSHEET_TYPES = List.of("application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 
+    public static boolean SUPPORTED_TYPES(String fileType) {
+        return SUPPORTED_IMAGE_TYPES.contains(fileType) || SUPPORTED_DOCUMENT_TYPES.contains(fileType) || SUPPORTED_SPREADSHEET_TYPES.contains(fileType);
+    }
 }
