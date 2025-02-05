01cb1a53e68287459f531a1b4978d1764ad0d125
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-05 14:11:57 +0900

    FEAT(jwt): Claim 추출 메서드 및 한 번에 모든 프로세스 실행시키는 메서드 추가

--- src/main/java/com/capstone/global/jwt/JwtUtil.java
+++ src/main/java/com/capstone/global/jwt/JwtUtil.java
@@ -1,7 +1,12 @@
 package com.capstone.global.jwt;
 
 
+import com.capstone.domain.auth.exception.InformationNotContainException;
+import com.capstone.domain.auth.exception.InvalidTokenException;
+import com.capstone.domain.auth.token.message.TokenMessages;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -22,64 +27,32 @@ public class JwtUtil {
     //검증 메서드
 
     public JwtUtil(@Value("${jwt.secret}") String secret) {
-        //SecretKeySpec 객체를 생성하여 secretKey 필드에 할당 -> SecretKeySpec 객체는 SecretKey 인터페이스를 구현하는 객체
-        //secret 문자열을 UTF-8 형식의 바이트 배열로 변환
-        //HMAC-SHA256 알고리즘을 설정하여 JWT 서명에 사용될 알고리즘을 지정 -> JWT 의 기본 알고리즘
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
-    public String getId(String token) {
-        /**
-         * Jwts.parser() -> Jwts parser 생성
-         * .verifyWith(secretKey) -> 토큰 검증에 사용할 비밀 키 설정 토큰 위 변조 검증이 이루어짐
-         * .build() 파서 구성을 완료하고, 검증을 위한 파서 객체를 생성
-         * .parseSignedClaims(token) JWT의 서명된 클레임(payload)을 추출하는 메서드
-         * .getPayload() 파싱된 클레임 객체에서 payload 부분을 가져옴
-         * .get("email", String.class)는 JWT의 페이로드에서 "email"이라는 이름의 클레임을 String 타입으로 가져엄
-         */
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
-                .get("customerId", String.class);
-    }
     public String getEmail(String token) {
-        /**
-         * Jwts.parser() -> Jwts parser 생성
-         * .verifyWith(secretKey) -> 토큰 검증에 사용할 비밀 키 설정 토큰 위 변조 검증이 이루어짐
-         * .build() 파서 구성을 완료하고, 검증을 위한 파서 객체를 생성
-         * .parseSignedClaims(token) JWT의 서명된 클레임(payload)을 추출하는 메서드
-         * .getPayload() 파싱된 클레임 객체에서 payload 부분을 가져옴
-         * .get("email", String.class)는 JWT의 페이로드에서 "email"이라는 이름의 클레임을 String 타입으로 가져엄
-         */
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
                 .get("email", String.class);
     }
 
-
-    public String getRole(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
-                .get("role", String.class);
-    }
-
-    //토큰 유효기간 만료 확인
     public Boolean isExpired(String token) {
-        /**
-         * .getExpiration()은 JWT의 만료 시간을 Date 객체로 반환합니다.
-         * .before(new Date())는 getExpiration()으로 가져온 만료 시간이 현재 시각(new Date()) 이전인지 확인
-         */
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
-                .getExpiration().before(new Date());
+        try {
+            Claims claims = extractClaims(token);
+            Date expiration = claims.getExpiration();
+
+            if (expiration == null) {
+                throw new InformationNotContainException(TokenMessages.INFORMATION_NOT_CONTAINED);
+            }
+
+            return expiration.before(new Date());
+        } catch (Exception e) {
+            throw new InvalidTokenException(TokenMessages.INVALID_TOKEN);
+        }
     }
 
-    //JWTToken 생성
-    public String createAccess(String customerId, String email) {
-        /**
-         * issuedAt 메서드로 토큰 발행 시각을 설정
-         * expiration 메서드로 토큰의 만료 시각 설정 현재시각 + expiredMs
-         * signWith(secretKey) -> secretKey를 사용에 토큰에 서명
-         * compact 메서드는 설정된 내용을 바탕으로 최종 JWT 토큰을 생성하고, 이를 String 형태로 반환
-         */
+    public String createAccess(String email) {
         return Jwts.builder()
                 .claim("category", "access")
-                .claim("customerId", customerId)
                 .claim("email", email)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRE_TIME))
@@ -87,20 +60,31 @@ public class JwtUtil {
                 .compact();
     }
 
-    public String createRefresh(String customerId, String email) {
-        /**
-         * issuedAt 메서드로 토큰 발행 시각을 설정
-         * expiration 메서드로 토큰의 만료 시각 설정 현재시각 + expiredMs
-         * signWith(secretKey) -> secretKey를 사용에 토큰에 서명
-         * compact 메서드는 설정된 내용을 바탕으로 최종 JWT 토큰을 생성하고, 이를 String 형태로 반환
-         */
+    public String createRefresh(String email) {
         return Jwts.builder()
                 .claim("category", "refresh")
-                .claim("customerId", customerId)
                 .claim("email", email)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRE_TIME))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    private Claims extractClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getBody();
+
+    }
+
+    public String processToken(String accessToken, HttpServletRequest request){
+        String refreshToken = CookieUtil.findTokenOrThrow(request);
+        if (!isExpired(accessToken) && !isExpired(refreshToken)){
+            return createRefresh(getEmail(accessToken));
+        }
+
+        return null;
     }
 }
