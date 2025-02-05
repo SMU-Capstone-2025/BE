86c85f766b8cf07b3deaaffeb14fbb57c42c3780
박기진 <dugudada01@naver.com>
2025-02-05 14:31:54 +0900

    FEAT(api): 채팅 내역 불러오기 추가

--- src/main/java/com/capstone/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/capstone/domain/chat/controller/ChatRoomController.java
@@ -1,7 +1,10 @@
 package com.capstone.domain.chat.controller;
 
+import com.capstone.domain.chat.dto.ChatRequest;
 import com.capstone.domain.chat.dto.ChatRoom;
 import com.capstone.domain.chat.repository.ChatRoomRepository;
+import com.capstone.domain.chat.service.ChatRoomService;
+import com.capstone.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +15,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatRoomController
 {
-    private final ChatRoomRepository chatRoomRepository;
+    private final ChatRoomService chatRoomService;
+    private final ChatService chatService;
 
     @GetMapping("/rooms")
-    public List<ChatRoom> getAllRooms() {
-        return chatRoomRepository.findAllRooms();
+    public List<ChatRoom> getAllRoomsService()
+    {
+        return chatRoomService.getRooms();
     }
 
     @GetMapping("/room/{roomId}")
-    public ChatRoom getRoom(@PathVariable String roomId) {
-        return chatRoomRepository.findRoomById(roomId);
+    public ChatRoom getRoomService(@PathVariable String roomId)
+    {
+        return chatRoomService.getRoom(roomId);
     }
 
     @PostMapping("/room")
-    public ChatRoom createRoom(@RequestParam String name) {
-        ChatRoom room = chatRoomRepository.createChatRoom(name);
+    public ChatRoom createRoomService(@RequestParam String name)
+    {
+        ChatRoom room = chatRoomService.createRoom(name);
         System.out.println("Created Room: " + room.getRoomId());  // 서버 로그 확인
         return room;
+    }
+    @GetMapping("/room/{roomId}/history")
+    public List<ChatRequest.ChatMessageDTO> getChatHistory(@PathVariable String roomId)
+    {
+        return chatService.getChatMessages(roomId,50);
     }
 }
