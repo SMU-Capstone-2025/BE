0a9da8495e8cd0137c26d56e250170377d909133
박기진 <dugudada01@naver.com>
2025-02-05 14:34:32 +0900

    FEAT: 채팅방 퇴장 로직 및 채팅 내용 저장/불러오기 추가

--- src/main/java/com/capstone/domain/chat/service/ChatService.java
+++ src/main/java/com/capstone/domain/chat/service/ChatService.java
@@ -10,13 +10,16 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Service
 public class ChatService {
 
     private final SimpMessagingTemplate messagingTemplate;
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, ChatRequest.ChatMessageDTO> redisTemplate;
     private final ObjectMapper objectMapper;
 
     public void processMessage(ChatRequest.ChatMessageDTO message, String chatRoomId)
@@ -25,8 +28,11 @@ public class ChatService {
             throw new ChatNotFoundException("채팅방을 찾을 수 없습니다.");
         }
 
+        saveChatMessage(chatRoomId, message);
+
         String userMessage = message.getContent();
         System.out.println("Processing message: " + userMessage + " Room ID: " + chatRoomId);
+
         redisTemplate.convertAndSend("chatRoom:" + chatRoomId, message);
     }
 
@@ -39,7 +45,34 @@ public class ChatService {
 
         message.setType(ChatRequest.ChatMessageDTO.MessageType.JOIN);
         message.setContent(message.getSender()+"님이 입장하였습니다.");
+
         redisTemplate.convertAndSend("chatRoom:" +chatRoomId, message);
+    }
+    public void exitMessage(ChatRequest.ChatMessageDTO message, SimpMessageHeaderAccessor headerAccessor, String chatRoomId)
+    {
+        if (chatRoomId == null || chatRoomId.isEmpty()) {
+            throw new ChatNotFoundException("채팅방을 찾을 수 없습니다.");
+        }
+
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+
+        message.setType(ChatRequest.ChatMessageDTO.MessageType.LEAVE);
+        message.setContent(message.getSender()+"님이 퇴장하였습니다.");
+        redisTemplate.convertAndSend("chatRoom:" +chatRoomId, message);
+    }
+
+    public void saveChatMessage(String roomId, ChatRequest.ChatMessageDTO message)
+    {
+        String key = "chat:room:" + roomId;
+        redisTemplate.opsForList().leftPush(key, message);
+        redisTemplate.opsForList().trim(key, 0, 49);
+        redisTemplate.expire(key, Duration.ofHours(1));
+    }
+    public List<ChatRequest.ChatMessageDTO> getChatMessages(String roomId, int limit)
+    {
+        String key = "chat:room:" + roomId;
+        List<ChatRequest.ChatMessageDTO> messages = redisTemplate.opsForList().range(key, 0, limit - 1);
+        return messages;
     }
 
 
