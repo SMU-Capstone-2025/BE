042573b97b6c0f3229b64418b55b0047df843e6c
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-06 20:51:29 +0900

    FEAT(entity): 프로젝트 엔티티 설정

---
+++ src/main/java/com/capstone/domain/project/entity/Project.java
@@ -0,0 +1,29 @@
+package com.capstone.domain.project.entity;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Map;
+
+@Document(collection= "project")
+@Getter
+@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+    @Id
+    private String id;
+    private String projectName;
+    private String description;
+    private Map<String, String> authorities; // email:authority 구조
+    private List<String> projectIds;
+    private List<String> documentIds;
+
+    public String getAuthority(String email){
+        return (authorities != null) ? authorities.get(email) : null;
+    }
+}
