b935d142d8783f691370bdd59d987eb119d9fdf4
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-07 21:33:14 +0900

    FEAT(dto): 프로젝트 id 필드 추가

--- src/main/java/com/capstone/domain/project/dto/ProjectDto.java
+++ src/main/java/com/capstone/domain/project/dto/ProjectDto.java
@@ -1,5 +1,6 @@
 package com.capstone.domain.project.dto;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProjectDto {
+    @Nullable
+    private String projectId;
     @NotBlank
     private String projectName;
     @NotBlank
