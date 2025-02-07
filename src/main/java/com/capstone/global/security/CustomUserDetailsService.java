e0f8833049041e40a5d0e9ed9d468bdfa6a51db1
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-07 21:50:16 +0900

    FEAT: 사용자의 특정 프로젝트의 역할 값을 권한에 추가

--- src/main/java/com/capstone/global/security/CustomUserDetailsService.java
+++ src/main/java/com/capstone/global/security/CustomUserDetailsService.java
@@ -1,15 +1,19 @@
 package com.capstone.global.security;
 
-import com.capstone.domain.auth.entity.User;
-import com.capstone.domain.auth.repository.UserRepository;
+import com.capstone.domain.project.entity.Project;
+import com.capstone.domain.user.entity.User;
+import com.capstone.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +21,20 @@ import java.util.Optional;
 public class CustomUserDetailsService implements UserDetailsService{
 
     private final UserRepository userRepository;
+    private final MongoTemplate mongoTemplate;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findUserByEmail(username);
-        return new CustomUserDetails(user);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        List<Project> projects = mongoTemplate.find(
+                new Query(Criteria.where("_id").in(user.getProjectIds())),
+                Project.class
+        );
+
+        return new CustomUserDetails(user, projects);
     }
 }
