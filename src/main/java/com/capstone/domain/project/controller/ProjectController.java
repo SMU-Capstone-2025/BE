850d5d012555f48591f671c2e62132fa41139a6e
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-07 21:32:28 +0900

    FEAT(api): 프로젝트 수정 및 권한 부여

--- src/main/java/com/capstone/domain/project/controller/ProjectController.java
+++ src/main/java/com/capstone/domain/project/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.capstone.domain.project.controller;
 
+import com.capstone.domain.project.dto.AuthorityRequest;
 import com.capstone.domain.project.dto.ProjectDto;
 import com.capstone.domain.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
@@ -15,5 +16,17 @@ public class ProjectController {
     @PostMapping("/register")
     public void registerProject(@RequestBody ProjectDto projectDto){
         projectService.processRegister(projectDto);
+    }
+
+    @PreAuthorize("ROLE_MANAGER")
+    @PutMapping("/update")
+    public void updateProject(@RequestBody ProjectDto projectDto){
+        projectService.processUpdate(projectDto);
+    }
+
+    @PreAuthorize("ROLE_MANAGER")
+    @PutMapping("/auth")
+    public void updateAuthority(@RequestBody AuthorityRequest authorityRequest){
+        projectService.processAuth(authorityRequest);
     }
 }
