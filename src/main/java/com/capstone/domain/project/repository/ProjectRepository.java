c74fa93f8a57638b28a556ba06502a73754934da
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-07 21:42:57 +0900

    FEAT(repo): 프로젝트 레포지토리

--- src/main/java/com/capstone/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/capstone/domain/project/repository/ProjectRepository.java
@@ -1,9 +1,10 @@
 package com.capstone.domain.project.repository;
 
 import com.capstone.domain.project.entity.Project;
+import com.capstone.domain.project.repository.custom.CustomProjectRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends MongoRepository<Project, String> {
+public interface ProjectRepository extends MongoRepository<Project, String>, CustomProjectRepository {
 }
