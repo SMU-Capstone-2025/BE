c623778d22920a4c30f0e4d24856af5adfa36bc9
Park Jinyoung <Jinyoung@MacBook-Pro.local>
2025-02-08 17:43:51 +0900

    FEAT(message): 접근 권한 예외에 띄울 메시지

--- src/main/java/com/capstone/domain/project/message/ProjectMessages.java
+++ src/main/java/com/capstone/domain/project/message/ProjectMessages.java
@@ -2,4 +2,5 @@ package com.capstone.domain.project.message;
 
 public class ProjectMessages {
     public static final String PROJECT_NOT_FOUND = "프로젝트를 찾을 수 없습니다.";
+    public static final String PROJECT_NOT_ACCESS = "프로젝트에 접근할 권한이 없습니다.";
 }
