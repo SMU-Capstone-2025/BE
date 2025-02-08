4f187ba7365dbeeb4869c35016bd082f566e09d8
박기진 <dugudada01@naver.com>
2025-02-08 18:12:37 +0900

    FEAT: 예외처리 수정

--- src/main/java/com/capstone/domain/chat/exception/MessageProcessingException.java
+++ src/main/java/com/capstone/domain/chat/exception/MessageProcessingException.java
@@ -4,4 +4,7 @@ public class MessageProcessingException extends RuntimeException {
     public MessageProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
+    public MessageProcessingException(String message) {
+        super(message);
+    }
 }
