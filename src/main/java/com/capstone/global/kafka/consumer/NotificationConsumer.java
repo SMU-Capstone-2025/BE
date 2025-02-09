082a2b33432495d7b4d5ad3f7696b9927414e602
Jinyoung <glitt5384@naver.com>
2025-02-09 22:21:43 +0900

    FEAT(kafka): 프로젝트 관련 이벤트 소비자

--- src/main/java/com/capstone/global/kafka/consumer/NotificationConsumer.java
+++ src/main/java/com/capstone/global/kafka/consumer/NotificationConsumer.java
@@ -2,18 +2,20 @@ package com.capstone.global.kafka.consumer;
 
 import com.capstone.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationConsumer {
     private final NotificationService notificationService;
 
     @KafkaListener(topics = "notification-event", groupId = "notification-group")
-    public void consumeNotificationMessage(String message){
+    public void consumeLogMessage(String message){
         try {
-            notificationService.processNotification(message);
+            notificationService.processLogNotification(message);
         } catch (Exception e) {
             System.err.println("Failed to process log message: " + e.getMessage());
         }
@@ -21,7 +23,7 @@ public class NotificationConsumer {
     @KafkaListener(topics = "update-event", groupId = "update-group")
     public void consumeUpdateMessage(String message){
         try {
-            notificationService.processNotification(message);
+            notificationService.processUpdateNotification(message);
         } catch (Exception e) {
             System.err.println("Failed to process log message: " + e.getMessage());
         }
