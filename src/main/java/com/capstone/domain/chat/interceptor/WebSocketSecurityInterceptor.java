1b5527111c55f4d365a5704d23a717cf4e92373e
박기진 <dugudada01@naver.com>
2025-02-25 14:16:52 +0900

    FIX: 모든 STOMP 메시지에서도 JWT 검증으로 변경

--- src/main/java/com/capstone/domain/chat/interceptor/WebSocketSecurityInterceptor.java
+++ src/main/java/com/capstone/domain/chat/interceptor/WebSocketSecurityInterceptor.java
@@ -27,34 +27,32 @@ public class WebSocketSecurityInterceptor implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        if (StompCommand.CONNECT == accessor.getCommand()) {
-            String token = accessor.getFirstNativeHeader("Authorization");
-            log.info("token{}", token);
+        String token = accessor.getFirstNativeHeader("Authorization");
+        log.info("token{}", token);
 
-            if (token != null) {
-                try {
-                    if (jwtUtil.isExpired(token)) {
-                        log.warn("JWT 토큰이 만료 되었습니다 - 재인증 요청 보내야합니다.");
-                        messagingTemplate.convertAndSendToUser(
-                                accessor.getSessionId(),
-                                "/queue/errors",
-                                "TOKEN_EXPIRED"
-                        );
-                        //이후에 프론트에서 웹소켓 재연결
-                        throw new IllegalArgumentException("JWT 토큰이 만료되었습니다.");
-                    }
-
-                    String name = jwtUtil.getName(token);
-                    accessor.getSessionAttributes().put("username",name);
-                    log.info(name);
-                } catch (Exception e) {
-                    log.error("JWT 검증 실패: {}", e.getMessage());
-                    throw new IllegalArgumentException("유효하지 않은 JWT 토큰입니다.");
+        if (token != null) {
+            try {
+                if (jwtUtil.isExpired(token)) {
+                    log.warn("JWT 토큰이 만료 되었습니다 - 재인증 요청 보내야합니다.");
+                    messagingTemplate.convertAndSendToUser(
+                            accessor.getSessionId(),
+                            "/queue/errors",
+                            "TOKEN_EXPIRED"
+                    );
+                    //이후에 프론트에서 웹소켓 재연결
+                    throw new IllegalArgumentException("JWT 토큰이 만료되었습니다.");
                 }
-            } else {
-                log.error("JWT 토큰이 없음");
-                throw new IllegalArgumentException("JWT 토큰이 필요합니다.");
+
+                String name = jwtUtil.getEmail(token);
+                accessor.getSessionAttributes().put("username",name);
+                log.info(name);
+            } catch (Exception e) {
+                log.error("JWT 검증 실패: {}", e.getMessage());
+                throw new IllegalArgumentException("유효하지 않은 JWT 토큰입니다.");
             }
+        } else {
+            log.error("JWT 토큰이 없음");
+            throw new IllegalArgumentException("JWT 토큰이 필요합니다.");
         }
 
         return message;
