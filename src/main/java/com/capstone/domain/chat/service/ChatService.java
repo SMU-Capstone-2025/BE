523eafffad2a92e96c5be73ed8ceb48686d01e16
박기진 <dugudada01@naver.com>
2025-02-25 19:40:54 +0900

    FIX: 일시적 메세지 저장 rightpush로 변경 및 토큰에서 username 추출

--- src/main/java/com/capstone/domain/chat/service/ChatService.java
+++ src/main/java/com/capstone/domain/chat/service/ChatService.java
@@ -3,6 +3,7 @@ package com.capstone.domain.chat.service;
 import com.capstone.domain.chat.dto.ChatRequest;
 import com.capstone.domain.chat.exception.ChatNotFoundException;
 import com.capstone.domain.chat.exception.MessageProcessingException;
+import com.capstone.global.jwt.JwtUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +19,16 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class ChatService {
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private final JwtUtil jwtUtil;
     private final RedisTemplate<String, ChatRequest.ChatMessageDTO> redisTemplate;
-    private final ObjectMapper objectMapper;
+
 
     public ChatRequest.ChatMessageDTO processMessage(ChatRequest.ChatMessageDTO message, String chatRoomId,SimpMessageHeaderAccessor headerAccessor)
     {
         if (chatRoomId == null || chatRoomId.isEmpty()) {
             throw new ChatNotFoundException("채팅방을 찾을 수 없습니다.");
         }
-
+        // 채팅 50개만 Redis에  저장 -> 채팅방 나갔다 들어와도 저장 되어있음
         saveChatMessage(chatRoomId, message);
 
         String userMessage = message.getContent();
@@ -36,7 +37,7 @@ public class ChatService {
         String name = (String) headerAccessor.getSessionAttributes().get("username");
 
         message.setSender(name);
-        message.setType(ChatRequest.ChatMessageDTO.MessageType.CHAT);
+
         redisTemplate.convertAndSend("chatRoom:" + chatRoomId, message);
         return message;
     }
@@ -46,15 +47,17 @@ public class ChatService {
         if (chatRoomId == null || chatRoomId.isEmpty()) {
             throw new ChatNotFoundException("채팅방을 찾을 수 없습니다.");
         }
+
+        //인터셉터에서 이미 이름 헤더에 저장
         String name=headerAccessor.getSessionAttributes().get("username").toString();
 
         message.setSender(name);
 
-        message.setType(ChatRequest.ChatMessageDTO.MessageType.JOIN);
         message.setContent(message.getSender()+"님이 입장하였습니다.");
 
         redisTemplate.convertAndSend("chatRoom:" +chatRoomId, message);
     }
+
     public void exitMessage(ChatRequest.ChatMessageDTO message, SimpMessageHeaderAccessor headerAccessor, String chatRoomId)
     {
         if (chatRoomId == null || chatRoomId.isEmpty()) {
@@ -64,7 +67,7 @@ public class ChatService {
         String name=headerAccessor.getSessionAttributes().put("username", message.getSender()).toString();
 
         message.setSender(name);
-        message.setType(ChatRequest.ChatMessageDTO.MessageType.LEAVE);
+
         message.setContent(message.getSender()+"님이 퇴장하였습니다.");
         redisTemplate.convertAndSend("chatRoom:" +chatRoomId, message);
     }
@@ -72,7 +75,7 @@ public class ChatService {
     public void saveChatMessage(String roomId, ChatRequest.ChatMessageDTO message)
     {
         String key = "chat:room:" + roomId;
-        redisTemplate.opsForList().leftPush(key, message);
+        redisTemplate.opsForList().rightPush(key, message);
         redisTemplate.opsForList().trim(key, 0, 49);
         redisTemplate.expire(key, Duration.ofHours(1));
     }
@@ -82,9 +85,6 @@ public class ChatService {
         List<ChatRequest.ChatMessageDTO> messages = redisTemplate.opsForList().range(key, 0, limit - 1);
         return messages;
     }
-
-
-
-
+    
 
 }
