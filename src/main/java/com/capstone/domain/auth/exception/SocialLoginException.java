e6d839904ad6ac870af2201cd5d217d398e8c1aa
Jinyoung <glitt5384@naver.com>
2025-02-28 02:32:54 +0900

    TEST: 쿠버네티스 + 로드밸런서

---
+++ src/main/java/com/capstone/domain/auth/exception/SocialLoginException.java
@@ -0,0 +1,7 @@
+package com.capstone.domain.auth.exception;
+
+public class SocialLoginException extends RuntimeException {
+    public SocialLoginException(String message) {
+        super(message);
+    }
+}
