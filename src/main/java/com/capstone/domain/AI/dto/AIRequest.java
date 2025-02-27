22c3c4acf13f51a47d32c2eb8bcb929be118092c
Jinyoung <glitt5384@naver.com>
2025-02-28 07:28:25 +0900

    ROLLBACK: 삭제된 AI 디렉토리 복구

---
+++ src/main/java/com/capstone/domain/AI/dto/AIRequest.java
@@ -0,0 +1,14 @@
+package com.capstone.domain.AI.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AIRequest
+{
+    private String request;
+}
