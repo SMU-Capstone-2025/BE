5def20de5e563bac4f46dffd286479b6a30887e3
Jinyoung <glitt5384@naver.com>
2025-03-07 00:56:01 +0900

    TEST: params가 아니라 message만 표시

--- document-service/src/main/java/domain/controller/DocumentController.java
+++ document-service/src/main/java/domain/controller/DocumentController.java
@@ -16,11 +16,15 @@ public class DocumentController {
     private static final Logger LOGGER = LoggerFactory.getLogger( DocumentController.class );
     private final SimpMessageSendingOperations messagingTemplate;
 
-    @MessageMapping("/editing")
+    // 클라이언트에서 "/pub/chat/message"로 메시지를 전송하면 실행됨
+    @MessageMapping("/chat/message")
     public void sendMessage(Map<String, Object> params) {
         String documentId = (String) params.get("documentId");
         String message = (String) params.get("message");
 
-        messagingTemplate.convertAndSend("/sub/chat/" + documentId, message);
+        LOGGER.info("메시지 수신 - 채널: {}, 내용: {}", documentId, message);
+
+        // 해당 채널을 구독 중인 모든 클라이언트에게 메시지 전송
+        messagingTemplate.convertAndSend("/sub/chat/" + documentId, params);
     }
 }
