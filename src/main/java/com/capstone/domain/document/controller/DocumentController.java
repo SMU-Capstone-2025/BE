d683d69e76dc1538b1182e0c325cc7d2cffa8ff9
Jinyoung <glitt5384@naver.com>
2025-03-08 03:22:54 +0900

    FEAT(api): /pub/editing 으로 요청을 보내면 같은 문서를 편집하는 클라이언트들에게 편집 내용 일괄 전송

--- src/main/java/com/capstone/domain/document/controller/DocumentController.java
+++ src/main/java/com/capstone/domain/document/controller/DocumentController.java
@@ -1,17 +1,14 @@
 package com.capstone.domain.document.controller;
 
+import com.capstone.domain.document.dto.DocumentEditRequest;
+import com.capstone.domain.document.dto.DocumentEditResponse;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.event.EventListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,10 +17,9 @@ public class DocumentController {
     private final SimpMessageSendingOperations messagingTemplate;
 
     @MessageMapping("/editing")
-    public void sendMessage(Map<String, Object> params) {
-        String documentId = (String) params.get("documentId");
-        String message = (String) params.get("message");
-
-        messagingTemplate.convertAndSend("/sub/chat/" + documentId, message);
+    public void sendMessage(DocumentEditRequest params,
+                            @Header("simpSessionAttributes") Map<String, Object> sessionAttributes) {
+        DocumentEditResponse documentEditResponse=  new DocumentEditResponse((String) sessionAttributes.get("email"), params.getMessage());
+        messagingTemplate.convertAndSend("/sub/chat/" + params.getDocumentId(), documentEditResponse);
     }
 }
