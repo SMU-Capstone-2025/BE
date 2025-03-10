af92e521c75c646f58f1e34ec36b8d64f17d759b
Jinyoung <glitt5384@naver.com>
2025-03-10 20:03:02 +0900

    FEAT(api): 문서 내용 불러오기 api

--- src/main/java/com/capstone/domain/document/controller/DocumentController.java
+++ src/main/java/com/capstone/domain/document/controller/DocumentController.java
@@ -2,10 +2,16 @@ package com.capstone.domain.document.controller;
 
 import com.capstone.domain.document.dto.DocumentEditRequest;
 import com.capstone.domain.document.dto.DocumentEditResponse;
+import com.capstone.domain.document.entity.Document;
+import com.capstone.domain.document.service.DocumentService;
+import com.capstone.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -15,11 +21,18 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class DocumentController {
     private final SimpMessageSendingOperations messagingTemplate;
+    private final DocumentService documentService;
+
+    @GetMapping("/load")
+    public Document getDocument(@RequestParam("documentId") String documentId){
+        return documentService.findDocumentCacheFirst(documentId);
+    }
 
     @MessageMapping("/editing")
     public void sendMessage(DocumentEditRequest params,
                             @Header("simpSessionAttributes") Map<String, Object> sessionAttributes) {
-        DocumentEditResponse documentEditResponse=  new DocumentEditResponse((String) sessionAttributes.get("email"), params.getMessage());
-        messagingTemplate.convertAndSend("/sub/chat/" + params.getDocumentId(), documentEditResponse);
+        DocumentEditResponse documentEditResponse = new DocumentEditResponse((String) sessionAttributes.get("email"), params.getMessage());
+        messagingTemplate.convertAndSend("/sub/document/" + params.getDocumentId(), documentEditResponse);
+        documentService.updateDocumentToCache(params.getDocumentId(), params.getMessage());
     }
 }
