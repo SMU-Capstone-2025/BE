ad2238713579b49f2cc07b06816c3744d9475bef
Jinyoung <glitt5384@naver.com>
2025-03-11 19:46:48 +0900

    FEAT: 커스텀 레포지토리 extends

--- src/main/java/com/capstone/domain/document/repository/DocumentRepository.java
+++ src/main/java/com/capstone/domain/document/repository/DocumentRepository.java
@@ -1,9 +1,10 @@
 package com.capstone.domain.document.repository;
 
 import com.capstone.domain.document.entity.Document;
+import com.capstone.domain.document.repository.custom.CustomDocumentRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentRepository extends MongoRepository<Document, String> {
+public interface DocumentRepository extends MongoRepository<Document, String>, CustomDocumentRepository {
 }
