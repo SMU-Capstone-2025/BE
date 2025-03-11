8913fc88d25c2c9b1dfb38c39fa77efdb1936ef8
Jinyoung <glitt5384@naver.com>
2025-03-11 19:46:20 +0900

    FEAT: JsonIgnoreProperties 추가; 역직렬화시 클래스 정보 무시

--- src/main/java/com/capstone/domain/document/entity/Document.java
+++ src/main/java/com/capstone/domain/document/entity/Document.java
@@ -1,5 +1,8 @@
 package com.capstone.domain.document.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +16,17 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Document {
     @Id
     private String id;
+    @Nullable
+    private String documentId; // 문서 조회 시 이용되는 값.
+    @Nullable
     private String content;
+    @Nullable
     private List<String> logs;
+    @Nullable
     private List<String> attachments;
 
     public Document(String id, String content){
