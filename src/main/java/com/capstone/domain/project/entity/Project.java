6dd626e6d40bdfea077c67974719240ea5badbdf
Jinyoung <glitt5384@naver.com>
2025-03-16 01:46:57 +0900

    REFACTOR(entity): 생성 및 업데이트 함수 서비스 레이어로 이전

--- src/main/java/com/capstone/domain/project/entity/Project.java
+++ src/main/java/com/capstone/domain/project/entity/Project.java
@@ -1,14 +1,9 @@
 package com.capstone.domain.project.entity;
 
-
-import com.capstone.domain.project.dto.AuthorityRequest;
-import com.capstone.domain.project.dto.ProjectDto;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,30 +21,4 @@ public class Project {
     private Map<String, String> authorities; // email:authority 구조
     private List<String> projectIds;
     private List<String> documentIds;
-
-    public String getAuthority(String email){
-        return (authorities != null) ? authorities.get(email) : null;
-    }
-
-    public static Project createProject(ProjectDto projectDto, Map<String, String> defaultAuthorities) {
-        return Project.builder()
-                .projectName(projectDto.getProjectName())
-                .description(projectDto.getDescription())
-                .authorities(defaultAuthorities)
-                .projectIds(new ArrayList<>())
-                .documentIds(new ArrayList<>())
-                .build();
-    }
-
-
-    public void updateAllAuthorities(Map<String, String> newAuthorities) {
-        newAuthorities.forEach(this::updateEachAuthority);
-    }
-
-    public void updateEachAuthority(String email, String role) {
-        if (this.authorities.containsKey(email)) {
-            this.authorities.replace(email, role);
-        }
-    }
-
 }
