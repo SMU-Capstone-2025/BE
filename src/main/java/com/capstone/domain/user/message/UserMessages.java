4f4c223086f3de3fd4f9bf6241ca44ff435846e0
웃기진않아 <dugudada01@naver.com>
2025-03-16 22:18:31 +0900

    FEAT: USER_FOUND 메시지 추가

--- src/main/java/com/capstone/domain/user/message/UserMessages.java
+++ src/main/java/com/capstone/domain/user/message/UserMessages.java
@@ -2,4 +2,5 @@ package com.capstone.domain.user.message;
 
 public class UserMessages {
     public static final String USER_NOT_FOUND = "해당 유저를 찾을 수 없습니다.";
+    public static final String USER_FOUND = "해당 유저가 이미 존재합니다.";
 }
