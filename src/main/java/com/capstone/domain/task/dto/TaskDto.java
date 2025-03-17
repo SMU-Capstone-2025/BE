e88590c166f97aa52674ef3566ea0f6aab3aa712
Jinyoung <glitt5384@naver.com>
2025-03-17 14:22:01 +0900

    REFACTOR(dto): toTask 함수 추가 및 record 변경

--- src/main/java/com/capstone/domain/task/dto/TaskDto.java
+++ src/main/java/com/capstone/domain/task/dto/TaskDto.java
@@ -1,23 +1,28 @@
 package com.capstone.domain.task.dto;
 
+import com.capstone.domain.task.entity.Task;
+import com.capstone.domain.task.message.Status;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class TaskDto {
-    @Nullable // 신규 작성 시에는 없음.
-    private String id; // taskId
-    @Nullable
-    private String title;
-    private String modifiedBy;
-    private String version;
-    private String summary;
-    private String content;
+public record TaskDto (String id,
+                       String title,
+                       String modifiedBy,
+                       String version,
+                       String summary,
+                       String content){
+
+    public Task toTask(TaskDto taskDto) {
+        return Task.builder()
+                .title(taskDto.title())
+                .status(Status.IN_PROGRESS)
+                .currentVersion(taskDto.version)
+                .versionHistory(new ArrayList<>())
+                .build();
+    }
 }
