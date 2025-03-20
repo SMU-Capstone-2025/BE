fd94118a82f13a3106e0ac72985f8d92f126cec9
웃기진않아 <dugudada01@naver.com>
2025-03-20 17:13:05 +0900

    FEAT(controller): 결제 검증 컨트롤러

---
+++ src/main/java/com/capstone/domain/payment/controller/PaymentController.java
@@ -0,0 +1,72 @@
+package com.capstone.domain.payment.controller;
+
+import com.capstone.domain.payment.dto.PaymentRequestDto;
+import com.capstone.domain.payment.exception.InvaildPaymentException;
+import com.capstone.domain.payment.service.PaymentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+import static com.capstone.domain.payment.message.PaymentMessages.IAMPORT_FAILDED;
+import static com.capstone.domain.payment.message.PaymentMessages.PAYMENT_NOT_FOUND;
+
+@RestController
+@RequestMapping("/api/v1/payment")
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentController
+{
+
+    private final PaymentService paymentService;
+
+    @Value("${IMP_API_KEY}")
+    private String apiKey;
+
+    @Value("${IMP_API_SECRET}")
+    private String secretKey;
+
+    private IamportClient iamportClient;
+
+    @PostConstruct
+    public void init()
+    {
+        this.iamportClient = new IamportClient(apiKey, secretKey);
+    }
+
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> validateIamport(@RequestHeader("Authorization") String token, @RequestBody PaymentRequestDto request) {
+        try {
+            IamportResponse<Payment> payment = iamportClient.paymentByImpUid(request.getImpUid());
+
+            if (payment == null || payment.getResponse() == null) {
+                throw new InvaildPaymentException(PAYMENT_NOT_FOUND);
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(payment.getResponse());
+            log.info("아임포트 응답: {}", jsonResponse);
+
+            paymentService.processPayment(token,request.getImpUid());
+
+            return ResponseEntity.ok(payment);
+
+        } catch (IamportResponseException e) {
+            log.error("아임포트 결제 검증 실패: {}", e.getMessage());
+            throw new InvaildPaymentException(IAMPORT_FAILDED + e.getMessage());
+        } catch (IOException e) {
+            log.error("JSON 변환 오류: {}", e.getMessage());
+            return ResponseEntity.internalServerError().body("서버 오류 발생: " + e.getMessage());
+        }
+    }
+}
