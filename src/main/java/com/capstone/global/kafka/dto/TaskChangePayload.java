7162115f6332fd08ae9f7e540bc9e3987bbb5324
Jinyoung <glitt5384@naver.com>
2025-03-20 19:45:35 +0900

    REFACTOR: TaskDto의 필드로 수정

--- src/main/java/com/capstone/global/kafka/dto/TaskChangePayload.java
+++ src/main/java/com/capstone/global/kafka/dto/TaskChangePayload.java
@@ -3,5 +3,10 @@ package com.capstone.global.kafka.dto;
 import java.util.List;
 import java.util.Map;
 
-public record TaskChangePayload(String target, Map<String, String> authorities, List<String> emails) {
+public record TaskChangePayload(String id,
+                                String title,
+                                String modifiedBy,
+                                String version,
+                                String summary,
+                                String content) {
 }
