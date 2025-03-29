3dea4b35bb12706b1efef87565e9f1fe8b442e78
웃기진않아 <dugudada01@naver.com>
2025-03-29 19:24:38 +0900

    REFACTOR: Custom 방식으로 변경

--- src/main/java/com/capstone/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/capstone/domain/payment/repository/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.capstone.domain.payment.repository;
 
 
 import com.capstone.domain.payment.entity.PaymentEntity;
+import com.capstone.domain.project.repository.custom.CustomProjectRepository;
 import com.siot.IamportRestClient.response.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PaymentRepository extends MongoRepository<PaymentEntity, String> {
-    List<PaymentEntity> findByUserEmail(String userEmail);
+public interface PaymentRepository extends MongoRepository<PaymentEntity, String> , CustomProjectRepository {
+
 }
 
