1bd665a7ce318613d5f5bdf5fd37632f8b1b0591
Jinyoung <glitt5384@naver.com>
2025-03-30 22:41:39 +0900

    FEAT: 프로젝트의 초대에 대한 알림을 담당하는 핸들러

---
+++ src/main/java/com/capstone/domain/project/handler/ProjectInviteHandler.java
@@ -0,0 +1,39 @@
+package com.capstone.domain.project.handler;
+
+import com.capstone.domain.notification.handler.NotificationHandler;
+import com.capstone.global.kafka.message.MessageGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class ProjectInviteHandler implements NotificationHandler {
+    @Override
+    public boolean canHandle(String method, String topic) {
+        return "INVITE".equals(method) && "PROJECT".equals(topic);
+    }
+
+    @Override
+    public String generateMessage(JsonNode rootNode) {
+        List<String> email = Collections.singletonList(rootNode.get("email").asText());
+        JsonNode data = rootNode.get("data");
+
+        Map<String, Object> merged = Map.of(
+                "email", Collections.singletonList(rootNode.get("email").asText()),
+                "title", rootNode.get("data").get("projectName").asText()
+        );
+
+        return MessageGenerator.generateFromDto(MessageGenerator.PROJECT_UPDATED, merged);
+    }
+
+    @Override
+    public List<String> findCoworkers(JsonNode rootNode) {
+        return Collections.singletonList(rootNode.get("email").asText());
+    }
+}
