753b3a6beaae6fb046e418eda08e60540831df96
Jinyoung <glitt5384@naver.com>
2025-03-30 22:41:28 +0900

    FEAT: 프로젝트의 권한 변경에 대한 알림을 담당하는 핸들러

---
+++ src/main/java/com/capstone/domain/project/handler/ProjectAuthHandler.java
@@ -0,0 +1,37 @@
+package com.capstone.domain.project.handler;
+
+import com.capstone.domain.notification.handler.NotificationHandler;
+import com.capstone.global.kafka.message.MessageGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ProjectAuthHandler implements NotificationHandler {
+    @Override
+    public boolean canHandle(String method, String topic) {
+        return "AUTH".equals(method) && "PROJECT".equals(topic);
+    }
+
+
+    @Override
+    public String generateMessage(JsonNode rootNode) {
+        List<String> email = Collections.singletonList(rootNode.get("email").asText());
+        JsonNode data = rootNode.get("data");
+
+        Map<String, Object> merged = Map.of(
+                "email", Collections.singletonList(rootNode.get("email").asText()),
+                "title", rootNode.get("data").get("projectName").asText()
+        );
+
+        return MessageGenerator.generateFromDto(MessageGenerator.PROJECT_UPDATED, merged);
+    }
+
+    @Override
+    public List<String> findCoworkers(JsonNode rootNode) {
+        return Collections.singletonList(rootNode.get("email").asText());
+    }
+}
