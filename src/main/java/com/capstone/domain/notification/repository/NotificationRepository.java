a4c8ae5f94ab9ec93c011988b11824794e7b148d
Jinyoung <glitt5384@naver.com>
2025-04-06 20:10:32 +0900

    REFACTOR: @Repository 어노테이션 추가

--- src/main/java/com/capstone/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/capstone/domain/notification/repository/NotificationRepository.java
@@ -3,6 +3,8 @@ package com.capstone.domain.notification.repository;
 import com.capstone.domain.notification.entity.Notification;
 import com.capstone.domain.notification.repository.custom.CustomNotificationRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends MongoRepository<Notification, String>, CustomNotificationRepository {
 }
