1c7024acd5528014ac1a4f8b8dbaa7d52ee2fc76
Jinyoung <glitt5384@naver.com>
2025-04-07 11:03:00 +0900

    FEAT: 성공 상태 코드

---
+++ src/main/java/com/capstone/global/response/status/SuccessStatus.java
@@ -0,0 +1,17 @@
+package com.capstone.global.response.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus {
+
+    // 공통 응답
+    _OK(HttpStatus.OK, "COMMON_200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
