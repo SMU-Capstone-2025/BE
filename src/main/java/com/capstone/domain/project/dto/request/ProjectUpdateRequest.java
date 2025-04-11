19a27084f96eab4e9f439055a1e737824d309763
Jinyoung <glitt5384@naver.com>
2025-04-12 05:57:02 +0900

    FEAT: 초대 이메일은 필요 없을 것 같아서 신규 생성

---
+++ src/main/java/com/capstone/domain/project/dto/request/ProjectUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.capstone.domain.project.dto.request;
+
+import com.capstone.domain.project.entity.Project;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record ProjectUpdateRequest (
+        @Nullable
+        String projectId,
+        @NotNull
+        @Size(min = 1)
+        String projectName,
+        @NotNull
+        @Size(min = 1)
+        String description
+){
+}
