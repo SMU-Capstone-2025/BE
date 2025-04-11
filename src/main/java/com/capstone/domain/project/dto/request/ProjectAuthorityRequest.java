551f03ae5b21cc17ca05ebadf2d57762a9bf1d3a
Jinyoung <glitt5384@naver.com>
2025-04-12 06:03:17 +0900

    FEAT: ProjectUser 생성 함수 추가

--- src/main/java/com/capstone/domain/project/dto/request/ProjectAuthorityRequest.java
+++ src/main/java/com/capstone/domain/project/dto/request/ProjectAuthorityRequest.java
@@ -1,9 +1,12 @@
 package com.capstone.domain.project.dto.request;
 
+import com.capstone.domain.user.entity.ProjectUser;
 import com.capstone.domain.user.entity.Role;
 import com.capstone.global.response.exception.GlobalException;
 import com.capstone.global.response.status.ErrorStatus;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,7 +18,19 @@ public record ProjectAuthorityRequest(
 )
 {
     public List<String> getAuthorityKeysAsList() {
-        return authorities == null ? List.of() : authorities.keySet().stream().collect(Collectors.toList());
+        return authorities == null ? List.of() : new ArrayList<>(authorities.keySet());
+    }
+
+    public List<ProjectUser> from() {
+        return authorities.entrySet().stream()
+                .map(entry -> ProjectUser.builder()
+                        .projectId(projectId)
+                        .userId(entry.getKey())
+                        .role(entry.getValue())
+                        .status("ACCEPTED")
+                        .joinedAt(LocalDate.now().toString())
+                        .build())
+                .toList();
     }
 
     public void validateRoles() {
