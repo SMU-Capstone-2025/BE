ccb2e22850f653e4359572af9f604785b12d6599
Jinyoung <glitt5384@naver.com>
2025-04-12 06:09:22 +0900

    FIX: 불필요한 변수 할당 제거

--- src/main/java/com/capstone/domain/project/handler/ProjectCreateHandler.java
+++ src/main/java/com/capstone/domain/project/handler/ProjectCreateHandler.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,15 +19,12 @@ public class ProjectCreateHandler implements NotificationHandler {
 
     @Override
     public String generateMessage(JsonNode rootNode) {
-        List<String> email = Collections.singletonList(rootNode.get("email").asText());
-        JsonNode data = rootNode.get("data");
-
         Map<String, Object> merged = Map.of(
                 "email", Collections.singletonList(rootNode.get("email").asText()),
-                "title", rootNode.get("data").get("projectName").asText()
+                "projectName", rootNode.get("data").get("name").asText()
         );
 
-        return MessageGenerator.generateFromDto(MessageGenerator.PROJECT_UPDATED, merged);
+        return MessageGenerator.generateFromDto(MessageGenerator.PROJECT_CREATED, merged);
     }
 
     @Override
