57f883fb8bf6bcac6c19bcb3a36978a23ca72781
Jinyoung <glitt5384@naver.com>
2025-04-12 06:04:14 +0900

    FEAT: PreAuthorize 커스텀, 매개변수 Dto 일부 변화

--- src/main/java/com/capstone/domain/task/controller/TaskController.java
+++ src/main/java/com/capstone/domain/task/controller/TaskController.java
@@ -11,6 +11,8 @@ import com.capstone.global.response.ApiResponse;
 import com.capstone.global.security.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,29 +24,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/task")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('MEMBER', 'MANAGER')")
-public class TaskController implements TaskControllerDocs {
 
+public class TaskController implements TaskControllerDocs {
     private final TaskService taskService;
 
     @PostMapping("/post")
+    @PreAuthorize("@projectAuthorityEvaluator.hasPermission(#taskDto.projectId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<Task>> postTask(@Valid @RequestBody TaskRequest taskDto) {
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.saveTask(taskDto)));
     }
 
     @GetMapping("/get")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<Version>> getTask(@RequestParam String taskId){
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.loadVersionContent(taskId)));
     }
 
     @DeleteMapping("/delete")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<Task>> deleteTask(
             @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestParam String id){
-        return ResponseEntity.ok(ApiResponse.onSuccess(taskService.dropTask(id, userDetails)));
+            @RequestParam String taskId){
+        return ResponseEntity.ok(ApiResponse.onSuccess(taskService.dropTask(taskId, userDetails)));
     }
 
     @PutMapping("/status")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<Task>> putStatus(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam String id,
@@ -53,6 +58,7 @@ public class TaskController implements TaskControllerDocs {
     }
 
     @PostMapping("/version/save")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<Version>> postVersion(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                             @Valid @RequestBody TaskRequest taskDto,
                                                             @RequestParam(value = "fileId", required = false) String fileId){
@@ -60,27 +66,33 @@ public class TaskController implements TaskControllerDocs {
     }
 
     @GetMapping("/version/list")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<List<Version>>> getVersionLists(@RequestParam String taskId){
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.listVersions(taskId)));
     }
 
     @GetMapping("/version/back")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<TaskResponse>> getVersionRollback(@RequestParam String taskId, @RequestParam String version){
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.rollbackVersion(taskId, version)));
     }
 
     @GetMapping("/log")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<List<LogEntity>>> getLogList(@RequestParam String taskId){
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.findLogsByTaskId(taskId)));
     }
 
 
     @GetMapping("/list/deadline")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<List<Task>>> getDeadlineList(@AuthenticationPrincipal CustomUserDetails userDetails)
     {
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.listByDeadLine(userDetails)));
     }
+
     @GetMapping("/list/get")
+    @PreAuthorize("@projectAuthorityEvaluator.hasTaskPermission(#taskId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
     public ResponseEntity<ApiResponse<List<Task>>> getList(@AuthenticationPrincipal CustomUserDetails userDetails)
     {
         return ResponseEntity.ok(ApiResponse.onSuccess(taskService.listTask(userDetails)));
