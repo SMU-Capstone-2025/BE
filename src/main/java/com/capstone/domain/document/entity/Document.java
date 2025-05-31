568271eda09e93ad824552ff1698adb8d2b31175
Jinyoung <glitt5384@naver.com>
2025-05-31 17:18:30 +0900

    feat: update 메서드 추가

--- src/main/java/com/capstone/domain/document/entity/Document.java
+++ src/main/java/com/capstone/domain/document/entity/Document.java
@@ -1,11 +1,13 @@
 package com.capstone.domain.document.entity;
 
+import com.capstone.domain.document.dto.DocumentEditVo;
 import com.capstone.global.entity.BaseDocument;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.annotation.Nullable;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @org.springframework.data.mongodb.core.mapping.Document(collection = "document")
@@ -33,9 +35,15 @@ public class Document extends BaseDocument {
     @Nullable
     private List<String> attachments;
 
-    public Document(String id, String content){
-        this.id = id;
-        this.content = content;
+    public void update(String key, String projectId, DocumentEditVo vo){
+        this.id = key;
+        this.projectId = projectId;
+        if (vo.getTitle() != null) this.title = vo.getTitle();
+        if (vo.getStatus() != null) this.status = vo.getStatus();
+        if (vo.getLogs() != null) this.logs = vo.getLogs();
+        if (vo.getAttachments() != null) this.attachments = vo.getAttachments();
+        if (vo.getContent() != null) this.content = vo.getContent();
+        this.updatedAt = LocalDateTime.now();
     }
 
 }
