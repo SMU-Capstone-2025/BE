8887c043bdce48ad0c90f7b1937384ebbe6a0093
Jinyoung <glitt5384@naver.com>
2025-06-08 03:51:54 +0900

    feat: 카프카 topic 정의

---
+++ src/main/java/com/capstone/global/kafka/topic/KafkaTopicProperties.java
@@ -0,0 +1,39 @@
+package com.capstone.global.kafka.topic;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "spring.kafka.topics")
+@Getter
+@Setter
+public class KafkaTopicProperties {
+    private String taskCreated;
+    private String taskUpdated;
+    private String taskDeleted;
+    private String projectCreated;
+    private String projectUpdated;
+    private String projectDeleted;
+
+    private final Map<KafkaEventTopic, String> topicMap = new EnumMap<>(KafkaEventTopic.class);
+
+    @PostConstruct
+    public void initMap() {
+        topicMap.put(KafkaEventTopic.TASK_CREATED, taskCreated);
+        topicMap.put(KafkaEventTopic.TASK_UPDATED, taskUpdated);
+        topicMap.put(KafkaEventTopic.TASK_DELETED, taskDeleted);
+        topicMap.put(KafkaEventTopic.PROJECT_CREATED, projectCreated);
+        topicMap.put(KafkaEventTopic.PROJECT_UPDATED, projectUpdated);
+        topicMap.put(KafkaEventTopic.PROJECT_DELETED, projectDeleted);
+    }
+
+    public String resolve(KafkaEventTopic topic) {
+        return topicMap.get(topic);
+    }
+}
