c25aa1530c01d9290ebc268d4ed197af3146fc07
Jinyoung <glitt5384@naver.com>
2025-06-08 03:54:17 +0900

    fix: String 대신 Dto 받도록 변경, create, updated, delete 리스너 추가

--- src/main/java/com/capstone/global/kafka/consumer/TaskConsumer.java
+++ src/main/java/com/capstone/global/kafka/consumer/TaskConsumer.java
@@ -1,14 +1,16 @@
 package com.capstone.global.kafka.consumer;
 
-import com.capstone.domain.log.service.LogService;
+import com.capstone.domain.log.service.TaskLogService;
 import com.capstone.domain.notification.service.NotificationService;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.capstone.global.kafka.dto.TaskChangePayload;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 
@@ -18,26 +20,44 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class TaskConsumer {
     private final ObjectMapper objectMapper;
-    private final LogService logService;
+    private final TaskLogService logService;
     private final NotificationService notificationService;
 
-    @KafkaListener(topics = "task.changed", groupId = "log-service")
-    public void processLogSave(String message) {
+    @KafkaListener(topics = "task.created", groupId = "log-service", containerFactory = "taskKafkaListenerContainerFactory")
+    public void processCreateLogSave(@Header("kafka_receivedTopic") String kafkaTopic,
+                               @Payload TaskChangePayload payload) {
         try {
-            JsonNode jsonNode = objectMapper.readTree(message);
-            log.info("jsonNode: {}", jsonNode);
-            logService.saveLogEntityFromJsonNode(jsonNode);
+            logService.saveLogEntityFromPayload(kafkaTopic, payload);
+        } catch (Exception e) {
+            System.err.println("메시지 처리 실패: " + e.getMessage());
+        }
+    }
+    @KafkaListener(topics = "task.updated", groupId = "log-service", containerFactory = "taskKafkaListenerContainerFactory")
+    public void processUpdateLogSave(@Header("kafka_receivedTopic") String kafkaTopic,
+                               @Payload TaskChangePayload payload) {
+        try {
+            logService.saveLogEntityFromPayload(kafkaTopic, payload);
+        } catch (Exception e) {
+            System.err.println("메시지 처리 실패: " + e.getMessage());
+        }
+    }
+    @KafkaListener(topics = "task.deleted", groupId = "log-service", containerFactory = "taskKafkaListenerContainerFactory")
+    public void processDeleteLogSave(@Header("kafka_receivedTopic") String kafkaTopic,
+                               @Payload TaskChangePayload payload) {
+        try {
+            logService.saveLogEntityFromPayload(kafkaTopic, payload);
         } catch (Exception e) {
             System.err.println("메시지 처리 실패: " + e.getMessage());
         }
     }
 
-    @KafkaListener(topics = "task.changed", groupId = "notification-service")
-    public void consumeUpdateMessage(String message){
-        try {
-            notificationService.processUpdateNotification(message);
-        } catch (Exception e) {
-            System.err.println("Failed to process log message: " + e.getMessage());
-        }
-    }
+//    @KafkaListener(topics = "task.changed", groupId = "notification-service")
+//    public void consumeUpdateMessage(String message){
+//        try {
+//            log.info("message2: {} ", message);
+//            notificationService.processUpdateNotification(message);
+//        } catch (Exception e) {
+//            System.err.println("Failed to process log message: " + e.getMessage());
+//        }
+//    }
 }
