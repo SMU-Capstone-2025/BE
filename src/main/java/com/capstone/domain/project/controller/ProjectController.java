bbbf769af26654885c5252dfb7f9fe582d77e919
Jinyoung <glitt5384@naver.com>
2025-06-20 06:32:48 +0900

    feat: AuthenticationPrincipal 추가

--- src/main/java/com/capstone/domain/project/controller/ProjectController.java
+++ src/main/java/com/capstone/domain/project/controller/ProjectController.java
@@ -48,16 +48,20 @@ public class ProjectController implements ProjectControllerDocs {
 
     @PutMapping("/auth")
     @PreAuthorize("@projectAuthorityEvaluator.hasPermission(#projectAuthorityRequest.projectId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
-    public ResponseEntity<ApiResponse<Project>> updateAuthority(@RequestBody ProjectAuthorityRequest projectAuthorityRequest){
+    public ResponseEntity<ApiResponse<Project>> updateAuthority(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestBody ProjectAuthorityRequest projectAuthorityRequest){
         projectAuthorityRequest.validateRoles();
-        return ResponseEntity.ok(ApiResponse.onSuccess(projectUserService.processAuth(projectAuthorityRequest)));
+        return ResponseEntity.ok(ApiResponse.onSuccess(projectUserService.processAuth(customUserDetails, projectAuthorityRequest)));
     }
 
     @PutMapping("/invite")
     @PreAuthorize("@projectAuthorityEvaluator.hasPermission(#projectAuthorityRequest.projectId, {'ROLE_MANAGER','ROLE_MEMBER'}, authentication)")
-    public ResponseEntity<ApiResponse<Project>> inviteProject(@RequestBody ProjectAuthorityRequest projectAuthorityRequest){
+    public ResponseEntity<ApiResponse<Project>> inviteProject(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestBody ProjectAuthorityRequest projectAuthorityRequest){
         projectAuthorityRequest.validateRoles();
-        return ResponseEntity.ok(ApiResponse.onSuccess(projectUserService.processInvite(projectAuthorityRequest)));
+        return ResponseEntity.ok(ApiResponse.onSuccess(projectUserService.processInvite(customUserDetails, projectAuthorityRequest)));
     }
 
     @GetMapping("/load")
