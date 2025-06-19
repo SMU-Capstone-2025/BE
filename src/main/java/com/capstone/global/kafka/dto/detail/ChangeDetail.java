69fd24af406d4444cb3a3cecb2656b6338907768
Jinyoung <glitt5384@naver.com>
2025-06-20 06:27:57 +0900

    refactor: title, coworkers 등 공통된 값 포함

--- src/main/java/com/capstone/global/kafka/dto/detail/ChangeDetail.java
+++ src/main/java/com/capstone/global/kafka/dto/detail/ChangeDetail.java
@@ -1,4 +1,15 @@
 package com.capstone.global.kafka.dto.detail;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class ChangeDetail {
+    private String title;
+    private Object coworkers;
 }
