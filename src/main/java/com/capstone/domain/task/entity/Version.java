02bc10c84cc0adfe0ffa3446ec4eeb48d88cab4b
Jinyoung <glitt5384@naver.com>
2025-06-29 20:33:11 +0900

    feat: 첨부파일 추가 로직 추가

--- src/main/java/com/capstone/domain/task/entity/Version.java
+++ src/main/java/com/capstone/domain/task/entity/Version.java
@@ -3,10 +3,10 @@ package com.capstone.domain.task.entity;
 import com.capstone.global.entity.BaseDocument;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +16,13 @@ public class Version extends BaseDocument{
     private String modifiedDateTime;
     private String modifiedBy;
     private String content;
-    private List<String> attachmentList; // 사진, 표 등 첨부 파일 리스트.
+    private List<String> attachmentList;
+
+    public void addAttachment(String fileId) {
+        if (attachmentList == null) {
+            attachmentList = new ArrayList<>();
+        }
+        attachmentList.add(fileId);
+    }
 }
 
