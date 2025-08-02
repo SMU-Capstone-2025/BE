9a3e6fccb47709965805342ad788a1678ac8e1fb
Jinyoung <glitt5384@naver.com>
2025-08-02 18:44:07 +0900

    feat: 프로젝트 기참여, 이미 초대 요청 받은 사용자에게 초대가 갔을 때 예외 메시지

--- src/main/java/com/capstone/domain/user/message/UserMessages.java
+++ src/main/java/com/capstone/domain/user/message/UserMessages.java
@@ -3,4 +3,6 @@ package com.capstone.domain.user.message;
 public class UserMessages {
     public static final String USER_NOT_FOUND = "해당 유저를 찾을 수 없습니다.";
     public static final String USER_FOUND = "해당 유저가 이미 존재합니다.";
+    public static final String PROJECT_USER_EXISTS = "해당 유저가 이미 프로젝트에 소속되어 있습니다.";
+    public static final String PENDING_USER_EXISTS = "해당 유저는 현재 초대 수락 대기 상태입니다.";
 }
