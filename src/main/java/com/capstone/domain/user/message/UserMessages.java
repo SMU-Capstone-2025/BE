d67a2c4e1d072f5c43109312bbc02f91bd6358ef
Jinyoung <glitt5384@naver.com>
2025-08-12 00:49:21 +0900

    feat: 무효 사용자 예외 및 메시지

--- src/main/java/com/capstone/domain/user/message/UserMessages.java
+++ src/main/java/com/capstone/domain/user/message/UserMessages.java
@@ -5,4 +5,5 @@ public class UserMessages {
     public static final String USER_FOUND = "해당 유저가 이미 존재합니다.";
     public static final String PROJECT_USER_EXISTS = "해당 유저가 이미 프로젝트에 소속되어 있습니다.";
     public static final String PENDING_USER_EXISTS = "해당 유저는 현재 초대 수락 대기 상태입니다.";
+    public static final String INVALID_USERS = "아래 사용자가 회원가입 되어 있지 않아 초대할 수 없습니다.";
 }
