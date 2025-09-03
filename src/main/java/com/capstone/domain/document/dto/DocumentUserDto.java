4ec48e9a70742c9078e33a65d114c20c056cc5a4
Jinyoung <glitt5384@naver.com>
2025-09-03 19:58:49 +0900

    feat: 사용자 정보를 담은 Dto

---
+++ src/main/java/com/capstone/domain/document/dto/DocumentUserDto.java
@@ -0,0 +1,16 @@
+package com.capstone.domain.document.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentUserDto {
+    private Long userId;
+    private String userName;
+    private String userEmail;
+}
