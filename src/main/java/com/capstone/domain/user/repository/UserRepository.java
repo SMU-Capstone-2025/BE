583f27e4575ca4622e4ae5e1b8408c2ed12b1688
Jinyoung <glitt5384@naver.com>
2025-09-07 07:10:07 +0900

    feat: 이메일로 사용자 전부 조회

--- src/main/java/com/capstone/domain/user/repository/UserRepository.java
+++ src/main/java/com/capstone/domain/user/repository/UserRepository.java
@@ -5,6 +5,10 @@ import com.capstone.domain.user.repository.custom.CustomUserRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String>, CustomUserRepository {
+    List<User> findAllByEmailIn(Collection<String> emails);
 }
